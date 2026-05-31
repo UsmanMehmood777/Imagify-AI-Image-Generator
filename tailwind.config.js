dcd96ad93bbcf196c2a69407292f18ff377e3ebd
Usman Mehmood <usmandangrous@gmail.com>
2026-05-31 13:59:30 +0500

    Usman Mehmood - navbar, home using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // If the GreatStack video adds custom fonts or colors later, 
+      // you will paste them here inside the extend object!
+    },
+  },
+  plugins: [],
+}
