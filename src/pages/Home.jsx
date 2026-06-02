225ab91b861d8e8d89a1489372d5a2ac4893bbe0
Usman Mehmood <usmandangrous@gmail.com>
2026-06-02 20:53:53 +0500

    Usman Mehmood- Complete Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
+import Generatebtn from '../components/Generatebtn'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
+      <Description/>
+      <Testimonials/>
+      <Generatebtn/>
     </div>
   )
 }
